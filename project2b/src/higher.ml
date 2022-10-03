b5fdad4b350b834b9c10762303e78430b4c04e92
gibsonmurray <gibmurrays@gmail.com>
2022-10-03 12:36:45 -0400

    contains_elem done

--- project2b/src/higher.ml
+++ project2b/src/higher.ml
@@ -4,7 +4,9 @@ open Funs
 (* Part 1: High Order Functions *)
 (********************************)
 
-let contains_elem lst e = failwith "unimplemented"
+let contains_elem lst e = 
+  if (fold (fun a x -> if e = x then a + 1 else a) 0 lst) > 0
+  then true else false;;
 
 let is_present lst x = failwith "unimplemented"
 
