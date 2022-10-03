0d3b24463652dba4373236caf3da4f2514b435d5
gibsonmurray <gibmurrays@gmail.com>
2022-10-03 15:38:04 -0400

    finished higher.ml

--- project2b/src/higher.ml
+++ project2b/src/higher.ml
@@ -14,9 +14,10 @@ let count_occ lst target =
   fold (fun a x -> if x = target then a + 1 else a) 0 lst;;
 
 let uniq lst = 
-  fold (fun a x -> if contains_elem a x then a else x::a) [] lst;;
+  fold (fun a x -> if contains_elem a x then a else x :: a) [] lst;;
 
 let assoc_list lst =
-  
+  fold (fun a x -> (x, (count_occ lst x)) :: a) [] (uniq lst);; 
 
-let ap fns args = failwith "unimplemented"
+let ap fns args = 
+  fold (fun a x -> a @ (map x args)) [] fns;;
