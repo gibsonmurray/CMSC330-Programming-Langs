f636f243760624c5f40182a75622df2fc1ef2d20
gibsonmurray <gibmurrays@gmail.com>
2022-10-03 14:59:44 -0400

    done uniq

--- project2b/src/higher.ml
+++ project2b/src/higher.ml
@@ -4,14 +4,19 @@ open Funs
 (* Part 1: High Order Functions *)
 (********************************)
 
-let contains_elem lst e = fold (fun a x -> a || x = e) false lst;;
+let contains_elem lst e =
+  fold (fun a x -> a || x = e) false lst;;
 
-let is_present lst x = map (fun e -> if x = e then 1 else 0) lst;;
+let is_present lst x =
+  map (fun e -> if x = e then 1 else 0) lst;;
 
-let count_occ lst target = failwith "unimplemented"
+let count_occ lst target = 
+  fold (fun a x -> if x = target then a + 1 else a) 0 lst;;
 
-let uniq lst = failwith "unimplemented"
+let uniq lst = 
+  fold (fun a x -> if contains_elem a x then a else x::a) [] lst;;
 
-let assoc_list lst = failwith "unimplemented"
+let assoc_list lst =
+  
 
 let ap fns args = failwith "unimplemented"
