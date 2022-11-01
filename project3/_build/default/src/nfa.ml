e68b4ab3b9604c8020f0505b20e25dbedd3a5b91
gibsonmurray <gibmurrays@gmail.com>
2022-10-31 22:26:44 -0400

    maybe done

--- project3/_build/default/src/nfa.ml
+++ project3/_build/default/src/nfa.ml
@@ -33,7 +33,7 @@ let explode (s: string) : char list =
 let rec move_help (delta: ('q, 's) transition list) (q: 'q) (s: 's option) : 'q list =
   match delta with
   | [] -> []
-  | (state, trans, next)::rest -> if (state = q && trans = s) then [next] else move_help rest q s;;
+  | (state, trans, next)::rest -> if (state = q && trans = s) then next::(move_help rest q s)  else move_help rest q s;;
 
 let rec move (nfa: ('q,'s) nfa_t) (qs: 'q list) (s: 's option) : 'q list = 
   match qs with
@@ -43,7 +43,9 @@ let rec move (nfa: ('q,'s) nfa_t) (qs: 'q list) (s: 's option) : 'q list =
 let rec ec_help (delta: ('q, 's) transition list) (q: 'q) : 'q list =
   match delta with
   | [] -> [q]
-  | (state, trans, next)::rest -> if (state = q && trans = None) then state::(ec_help delta next) else ec_help rest q;;
+  | (state, trans, next)::rest -> if (state = q && trans = None)
+                                  then state::(union (ec_help rest q) (ec_help delta next)) 
+                                  else ec_help rest q;;
 
 let rec e_closure (nfa: ('q,'s) nfa_t) (qs: 'q list) : 'q list =
   match qs with
@@ -53,13 +55,14 @@ let rec e_closure (nfa: ('q,'s) nfa_t) (qs: 'q list) : 'q list =
 let rec accept_help nfa cs qs: bool = 
   match cs with
   | [] -> if (intersection qs nfa.fs) != [] then true else false
-  | c::rest -> (let nexts = (move nfa qs (Some c)) in 
-                if nexts != [] then (accept_help nfa rest nexts)
-                else if (intersection qs nfa.fs) != [] then true else false);;
+  | c::rest ->  let nexts = (move nfa qs (Some c)) in
+                if e_closure nfa nexts != []
+                then accept_help nfa rest (e_closure nfa nexts)
+                else false;;
 
 let accept (nfa: ('q, char) nfa_t) (s: string) : bool =
   if s = "" then if (intersection (e_closure nfa [nfa.q0]) nfa.fs) != [] then true else false
-  else accept_help nfa (explode s) [nfa.q0];;
+  else accept_help nfa (explode s) (e_closure nfa [nfa.q0]);;
 
 (*******************************)
 (* Part 2: Subset Construction *)
@@ -74,25 +77,30 @@ let new_trans (nfa: ('q,'s) nfa_t) (qs: 'q list) : ('q list, 's) transition list
 let new_finals (nfa: ('q,'s) nfa_t) (qs: 'q list) : 'q list list =
   fold_left (fun a x -> if elem x nfa.fs then [qs] else a) [] qs;;
 
-let rec nfa_to_dfa_step (nfa: ('q,'s) nfa_t) (dfa: ('q list, 's) nfa_t) (* TODO: change the prototype *)
-    (partial: 'q list list) (full: 'q list list): ('q list, 's) nfa_t =
-  match (diff full partial) with
+let rec nfa_to_dfa_step (nfa: ('q,'s) nfa_t) (dfa: ('q list, 's) nfa_t)
+    (work: 'q list list) : ('q list, 's) nfa_t =
+  match work with
   | [] -> dfa
-  | q::rest -> (let full = (union full (new_states nfa q)) in
-                (nfa_to_dfa_step nfa { sigma = dfa.sigma;
-                                        qs = full;
-                                        q0 = dfa.q0;
-                                        fs = [];
-                                        delta = (union (new_trans nfa q) dfa.delta)
-                                      } (q::partial) full))
+  | q::rest ->  match dfa with
+                | {sigma = sigmuh; qs = qss; q0 = initial; fs = finals; delta = trans} ->
+                  (let new_qs = (new_states nfa q) in
+                  let new_dfa = { 
+                                  sigma = sigmuh; 
+                                  qs = union new_qs qss; 
+                                  q0 = initial;
+                                  fs = union (new_finals nfa q) finals;
+                                  delta = union (new_trans nfa q) trans;
+                                } in
+                  nfa_to_dfa_step nfa new_dfa (diff (union new_qs work) [q]));;
 
 let nfa_to_dfa (nfa: ('q,'s) nfa_t) : ('q list, 's) nfa_t =
-  (let dfa = (nfa_to_dfa_step nfa {sigma = nfa.sigma; qs = []; q0 = [nfa.q0]; fs = []; delta = []} [] [e_closure nfa [nfa.q0]]) in
-    (match dfa with 
-    { sigma = sigmuh;
-      qs = qss;
-      q0 = initial;
-      fs = finals;
-      delta = trans
-    } -> {sigma = sigmuh; qs = qss; q0 = initial; fs = (intersection (new_finals nfa initial) [nfa.fs]); delta = trans}));; 
-                        (* TODO: may not need intersection ^ above *)
+  let initial = e_closure nfa [nfa.q0] in
+  nfa_to_dfa_step nfa 
+  {
+    sigma = nfa.sigma;
+    qs = [initial]; 
+    q0 = initial; 
+    fs = (new_finals nfa [nfa.q0]); 
+    delta = []
+  } 
+  [e_closure nfa [nfa.q0]];;
--- project3/src/nfa.mli
+++ project3/src/nfa.mli
@@ -30,6 +30,6 @@ val new_trans : ('q, 's) nfa_t -> 'q list -> ('q list, 's) transition list
 val new_finals : ('q, 's) nfa_t -> 'q list -> 'q list list
 
 val nfa_to_dfa_step :
-  ('q, 's) nfa_t -> ('q list, 's) nfa_t -> 'q list list -> 'q list list -> ('q list, 's) nfa_t
+  ('q, 's) nfa_t -> ('q list, 's) nfa_t -> 'q list list -> ('q list, 's) nfa_t
 
 val nfa_to_dfa : ('q, 's) nfa_t -> ('q list, 's) nfa_t
