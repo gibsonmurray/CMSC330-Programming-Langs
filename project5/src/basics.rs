59541b1368f05a6040801aefbb9dc54fb3b8f161
gibsonmurray <gibmurrays@gmail.com>
2022-12-05 14:25:04 -0500

    finished basics

--- project5/src/basics.rs
+++ project5/src/basics.rs
@@ -3,7 +3,16 @@
     If n is less than 0, return -1
 **/
 pub fn gauss(n: i32) -> i32 {
-    unimplemented!()
+    if n < 0 {
+        -1
+    }
+    else {
+        let mut x = 0;
+        for i in 1..(n + 1) {
+            x += i;
+        }
+        x
+    }
 }
 
 /**
@@ -11,7 +20,13 @@ pub fn gauss(n: i32) -> i32 {
     are in the range [s,e]
 **/
 pub fn in_range(ls: &[i32], s: i32, e: i32) -> i32 {
-    unimplemented!()
+    let mut ans = 0;
+    for &i in ls.iter() {
+        if i >= s && i <= e {
+            ans += 1;
+        }
+    }
+    ans
 }
 
 /**
@@ -20,7 +35,18 @@ pub fn in_range(ls: &[i32], s: i32, e: i32) -> i32 {
     Ex: [1,3,2] is a subset of [1,2,3,4,5]
 **/
 pub fn subset<T: PartialEq>(set: &[T], target: &[T]) -> bool {
-    unimplemented!()
+    for i in target.iter() {
+        let mut flag = false;
+        for j in set.iter() {
+            if &i == &j {
+                flag = true;
+            }
+        }
+        if !flag {
+            return flag;
+        }
+    }
+    return true;
 }
 
 /**
@@ -28,7 +54,18 @@ pub fn subset<T: PartialEq>(set: &[T], target: &[T]) -> bool {
     It might be helpful to use the fold method of the Iterator trait
 **/
 pub fn mean(ls: &[f64]) -> Option<f64> {
-    unimplemented!()
+    let avg = 
+    if ls.is_empty() {
+        None
+    }
+    else {
+        let mut sum = 0.0;
+        for &i in ls.iter() {
+            sum += i;
+        }
+        Some (sum/(ls.len() as f64))
+    };
+    avg
 }
 
 /**
@@ -37,7 +74,17 @@ pub fn mean(ls: &[f64]) -> Option<f64> {
     Ex: to_decimal of [1,0,1,0] returns 10
 **/
 pub fn to_decimal(ls: &[i32]) -> i32 {
-    unimplemented!()
+    let mut ans = 0;
+    let mut power = (ls.len() as i32) - 1;
+    let mut i = 0;
+    while power >= 0 {
+        if ls[i] == 1 {
+            ans += 2_i32.pow(power as u32);
+        }
+        power -= 1;
+        i += 1;
+    }
+    ans
 }
 
 /**
@@ -47,7 +94,22 @@ pub fn to_decimal(ls: &[i32]) -> i32 {
     Ex: factorize of 36 should return [2,2,3,3] since 36 = 2 * 2 * 3 * 3
 **/
 pub fn factorize(n: u32) -> Vec<u32> {
-    unimplemented!()
+    let mut x = n;
+    let mut vec = vec![];
+    while x % 2 == 0 {
+        vec.push(2);
+        x /= 2;
+    }
+    for i in 3..(x as f64).sqrt() as u32 + 1 {
+        while x % i == 0 {
+            vec.push(i);
+            x /= i;
+        }
+    }
+    if x > 2 {
+        vec.push(x);
+    }
+    vec
 }
 
 /** 
@@ -58,7 +120,12 @@ pub fn factorize(n: u32) -> Vec<u32> {
     EX: rotate [1,2,3,4] returns [2,3,4,1]
 **/
 pub fn rotate(lst: &[i32]) -> Vec<i32> {
-    unimplemented!()
+    let mut ans = vec![];
+    for i in 1..lst.len() {
+        ans.push(lst[i]);
+    }
+    ans.push(lst[0]);
+    ans
 }
 
 /**
@@ -68,7 +135,20 @@ pub fn rotate(lst: &[i32]) -> Vec<i32> {
     Ex: "ace" is a substring of "rustacean"
 **/
 pub fn substr(s: &String, target: &str) -> bool {
-    unimplemented!()
+    for i in 0..s.len() {
+        if s.chars().nth(i).unwrap() == target.chars().nth(0).unwrap() {
+            let mut flag = true;
+            for j in 1..target.len() {
+                if s.chars().nth(j).unwrap() != target.chars().nth(j).unwrap() {
+                    flag = false;
+                }
+            }
+            if flag {
+                return true
+            }
+        }
+    }
+    false
 }
 
 /**
@@ -80,5 +160,29 @@ pub fn substr(s: &String, target: &str) -> bool {
     EX: longest_sequence of "" is None
 **/
 pub fn longest_sequence(s: &str) -> Option<&str> {
-    unimplemented!()
+    if s == "" {
+        return None;
+    }
+    let mut starts = vec![];
+    let mut curr = &s[0..1];
+    starts.push(0);
+    for i in 1..s.len() {
+        if &s[i..(i + 1)] != curr {
+            starts.push(i);
+            curr = &s[i..(i + 1)];
+        }
+    }
+    let mut max_diff = 0;
+    let mut x = 0;
+    let mut y = 0;
+    for i in 0..(starts.len() - 1) {
+        let length = starts[i + 1] - starts[i];
+        if length > max_diff {
+            max_diff = length;
+            x = starts[i];
+            y = starts[i + 1];
+        }
+    }
+    Some(&s[x..y])
+
 }
